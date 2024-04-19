caac758df749bdf9c1b7cefa53dda022073534ba
Nélio Dias <jrneliodias@gmail.com>
2024-04-19 17:39:09 -0300

    style: the NavBar components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,17 @@
+"use client"
+import { cn } from "@/lib/utils";
 import MenuItem from "./menu/elements/MenuItem";
+import UseScrollPos from "@/hooks/useScrollPos";
 
 export default function Navbar() {
+    const isAtTop = UseScrollPos();
+
     return (
-        <div className="flex justify-evenly items-center bg-black h-14 text-white">
+        <div className={cn(
+            `sticky -mt-14 top-0 z-50 flex justify-around items-center bg-black h-14 text-white`,
+            `${isAtTop ? "opacity-100"
+                : "opacity-0 hover:opacity-100 transition-opacity duration-500"}`,
+        )}>
             <MenuItem title="About" />
             <MenuItem title="Projects" />
             <MenuItem title="Hero" />
