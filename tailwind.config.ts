572c79209bdce12ef0aa3ec2e038e5911b0cb2ce
Nélio Dias <jrneliodias@gmail.com>
2024-04-19 17:36:47 -0300

    feat: insert animation of photo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,8 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}", "./static/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
@@ -67,14 +62,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        scalepulse: {
+          "0%": { scale: "1" },
+          "50%": { scale: "1.01" },
+          "100%": { scale: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        scalepulse: "scalepulse 8s ease-in-out infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
