611370b7d26146eb274d414f711df5732a1fc10c
Nélio Dias <jrneliodias@gmail.com>
2024-04-21 21:47:58 -0300

    feat: create ProjectCard Component

---
+++ app/components/menu/elements/ProjectCard.tsx
@@ -0,0 +1,48 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Tag from "@/static/Tag";
+import Image from "next/image";
+
+type ProjectCardProps = {
+    title: string
+    text: string
+    image: string
+    link: string
+    tags: string[]
+}
+
+const ProjectCard = ({
+    title,
+    text,
+    image,
+    link,
+    tags
+}: ProjectCardProps) => {
+
+    return (
+        <Card className="bg-black shadow-sm text-white max-w-80 scale-100 transition-all hover:scale-[101%] bg-opacity-20 overflow-hidden">
+            <Image
+                src={image}
+                height={300}
+                width={400}
+                alt="next"
+            />
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <p>{text}</p>
+            </CardContent>
+            <CardFooter>
+                <div className="flex flex-wrap gap-2">
+                    {tags.map((tag, index) => (
+                        <div
+                            key={"t" + index}>
+                            <Tag title={tag} />
+                        </div>))}
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export default ProjectCard
