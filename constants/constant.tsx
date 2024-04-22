96b70fafc6790e616b3eaf01584d7c536a95981e
Nélio Dias <jrneliodias@gmail.com>
2024-04-21 21:49:33 -0300

    refactor: isolate the Timeline and Project Data

---
+++ constants/constant.tsx
@@ -0,0 +1,35 @@
+
+export const timelineData = [
+    {
+        title: "Fullstack Developer",
+        company: "Freelancing",
+        tasks: "Building modern web applications with API integration such as landing pages and dashboards",
+        tags: ["Next.Js", "React.Js", "Typescript", "REST API"],
+        year: "2024"
+    },
+    {
+        title: "Backend Developer",
+        company: "Tech Solutions Inc.",
+        tasks: "Developing RESTful APIs and database management for enterprise applications",
+        tags: ["Node.js", "Express.js", "MongoDB", "REST API"],
+        year: "2023"
+    },
+
+]
+
+export const projectData = [
+    {
+        title: "Fullstack Developer",
+        image: "/download.jpeg",
+        link: "https://www.github.com/jrneliodias",
+        text: "Building modern web applications with API integration such as landing pages and dashboards",
+        tags: ["Next.Js", "React.Js", "Typescript", "REST API"],
+    },
+    {
+        title: "Fullstack Developer",
+        image: "/download.jpeg",
+        link: "https://www.github.com/jrneliodias",
+        text: "Building modern web applications with API integration such as landing pages and dashboards",
+        tags: ["Next.Js", "React.Js", "Typescript", "REST API"],
+    },
+]
