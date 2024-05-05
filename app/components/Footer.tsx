84646987d25340c9587ccbb3b6143d40cd61c7ca
Nélio Dias <jrneliodias@gmail.com>
2024-05-04 22:23:15 -0300

    feat: add footer social media component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,12 @@
+import SocialMedia from "@/static/SocialMedia";
+
 export default function Footer() {
     return (
         <footer className="bg-gradient-to-r from-amber-600 to-slate-500">
 
-            <div id="Footer" className="flex justify-center items-center p-4 bg-gray-800 h-max text-white">
-                Footer
+            <div id="Footer" className="flex  flex-col gap-2 justify-center items-center p-4 bg-gray-800 h-max text-white">
+                <p>Contatos</p>
+                <SocialMedia size={30} />
             </div>
         </footer>
     )
