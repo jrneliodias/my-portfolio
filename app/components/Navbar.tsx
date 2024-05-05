dc7522db177b19341be8e84795e14177622edf1d
Nélio Dias <jrneliodias@gmail.com>
2024-05-05 12:17:40 -0300

    feat: add hamburguer component to mobile view

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,21 +2,78 @@
 import { cn } from "@/lib/utils";
 import MenuItem from "./menu/elements/MenuItem";
 import UseScrollPos from "@/hooks/useScrollPos";
+import { useEffect, useState } from "react";
+import useDeviceSize from "@/hooks/useDeviceSize";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import SocialMedia from "../static/SocialMedia";
 
 export default function Navbar() {
     const isAtTop = UseScrollPos();
+    const [isOpen, setIsOpen] = useState(false)
+    const [windowsWidth, _] = useDeviceSize()
+
+    const handleOpenSheet = () => setIsOpen(!isOpen)
 
     return (
-        <nav className={cn(
-            `sticky -mt-14 top-0 z-50 flex justify-around items-center bg-black h-14 text-white`,
-            `${isAtTop ? "opacity-100"
-                : "opacity-0 hover:opacity-100 transition-opacity duration-500"}`,
-        )}>
-            <div />
-            <div />
-            <MenuItem title="About" />
-            <MenuItem title="Projects" />
-            <MenuItem title="Services" />
-        </nav>
+        <>
+            {windowsWidth > 400 ? (<nav className={cn(
+                `sticky -mt-14 top-0 z-50 flex justify-around items-center bg-black h-14 text-white`,
+                `${isAtTop ? "opacity-100"
+                    : "opacity-0 hover:opacity-100 transition-opacity duration-500"}`,
+            )}>
+                <div />
+                <div />
+                <MenuItem title="About" />
+                <MenuItem title="Projects" />
+                <MenuItem title="Services" />
+            </nav>)
+                : (<nav className={cn(
+                    `sticky -mt-14 top-0 z-50 flex justify-end items-center px-3 bg-black/40 h-14 text-white `,
+
+                )}>
+                    <Button
+                        onClick={handleOpenSheet}
+                        size={'icon'}>
+                        <Menu />
+
+                    </Button>
+
+                    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+                        <SheetContent className="flex flex-col  justify-between pt-12 bg-slate-700 text-white ">
+                            <div className="flex flex-col">
+                                <Button
+                                    variant={'ghost'}
+                                    className="justify-start"
+                                    onClick={handleOpenSheet}
+                                >
+                                    <MenuItem title="About" />
+
+                                </Button>
+                                <Button
+                                    variant={'ghost'}
+                                    className="justify-start"
+                                    onClick={handleOpenSheet}
+                                >
+                                    <MenuItem title="Projects" />
+                                </Button>
+                                <Button
+                                    variant={'ghost'}
+                                    className="justify-start"
+                                    onClick={handleOpenSheet}
+                                >
+                                    <MenuItem title="Services" />
+                                </Button>
+                            </div>
+
+                            <SocialMedia size={30} />
+
+                        </SheetContent>
+                    </Sheet>
+                </nav >
+                )
+            }
+        </>
     )
 }
