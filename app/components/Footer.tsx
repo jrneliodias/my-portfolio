5f9e86ed645e36250d85d57ad41fbb06b0760752
Nelio Dias <jrneliodias@gmail.com>
2026-06-24 18:19:54 -0300

    feat: update Footer gradient and light mode styles

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,9 +5,12 @@ export default async function Footer() {
   const t = await getTranslations('footer');
 
   return (
-    <footer className="bg-gradient-to-r from-amber-600 to-slate-500">
-      <div id="Footer" className="flex flex-col gap-2 justify-center items-center p-4 bg-gradient-to-b from-slate-900 to-slate-800 h-max text-white">
-        <p>{t('contact')}</p>
+    <footer className="bg-gradient-to-r from-amber-500 to-violet-600 dark:from-amber-600 dark:to-slate-500">
+      <div
+        id="Footer"
+        className="flex flex-col gap-2 justify-center items-center p-8 bg-gradient-to-b from-slate-100 to-white dark:from-slate-900 dark:to-slate-800 h-max text-slate-900 dark:text-white"
+      >
+        <p className="font-display font-semibold text-lg">{t('contact')}</p>
         <SocialMedia size={30} />
       </div>
     </footer>
